9f3a3b6116108efea6379453f6f4d657e13f5985
silvio Molina <molinahuel44@gmail.com>
2020-12-15 23:04:22 -0300

    first little demostration of what i know about java, it's nothing but i hope i will increase with time

---
+++ main.java
@@ -0,0 +1,26 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Choose an option below!");
+        System.out.println("1.Hello");
+        System.out.println("2.what's going on");
+        System.out.println("3.hello! what's time is?");
+        
+        if(opc == '3'){
+          int resultant_hour = hour_discover();
+          printf("The hour is.. something");
+        };
+        
+        if(opc == '1'){
+          printf("Hi! my friend, you're not alone! little shit");
+        };
+        
+        if(opc == '2'){
+          printf("i trying to create an UX confortable for you");
+        };
+    }
+}
+
+
+
+
